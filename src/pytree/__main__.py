125e0e0b76eb8b2073690575457dd78aa7624e61
Sawors <yt.sawors@gmail.com>
2024-01-12 21:32:40 +0100

    made the project a working python package

--- src/pytree/__main__.py
+++ src/pytree/__main__.py
@@ -1,10 +1,10 @@
 #!/bin/env python3
 import os
-from pytree.Tree import Tree
-from pytree.Tree import Node
+from pytree.pytree import Tree
+from pytree.pytree import Node
 
-dir_icon = "󰉋 "
-tree_root_icon = "󰙅 "
+DIR_ICON = "󰉋 "
+TREE_ROOT_ICON = "󰙅 "
 icons = {
     "default": "󰈔 ",".txt": "󰈙 ", "": "",
     # images
@@ -39,30 +39,28 @@ icons = {
 def main():
     """Print a tree from the file system."""
     args = os.sys.argv[1:len(os.sys.argv)]
-    
-    for index, arg in enumerate(args):
-        pass
-    
+
     def get_icon(node:Node) -> str:
-        if Node.PRINT_NO_ICON_HINT in node.print_hints: return ""
+        if Node.PRINT_NO_ICON_HINT in node.print_hints:
+            return ""
         if node.is_root():
-            return tree_root_icon
-        if len(node._children) > 0:
-            return dir_icon
+            return TREE_ROOT_ICON
+        if len(node.get_children()) > 0:
+            return DIR_ICON
         name = node.name.lower()
         extension = name
         if "." in name:
             extension = name[name.rfind("."):len(name)]
         return icons[extension] if extension in icons else icons["default"]
-    
+
     dir_arg = [c for c in args if not c.startswith("-")]
     if len(dir_arg) > 0 and os.path.isdir(dir_arg[0]):
-        fs_tree = Tree.fs_tree(dir_arg[0], recursive=("-R" in args), show_dir=True)
+        fs_tree = Tree.fs_tree(dir_arg[0], recursive=("-R" in args), show_dir=True, full_path=("-f" in args))
         root = fs_tree.get_root()
         nodes = root.get_all_inheritance()
         root.name = get_icon(root)+root.name
         for node in nodes:
-           node.name = get_icon(node)+node.name 
+            node.name = get_icon(node)+node.name
         print(fs_tree)
 
 if __name__ == "__main__":
